c430ff168e1476cc35ec52190f4fc526fa4eea3b
Chris Parsons <chris@thinkcodelearn.com>
2014-01-02 22:57:46 +0000

    Form for news items

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource("news");
+  this.resource("news", function() {
+    this.resource("news-item", { path: ':news_item_id' });
+  });
   this.resource("talks");
+});
+
+App.NewsRoute = Ember.Route.extend({
+  model: function() {
+    return EmberFire.Array.create({
+      ref: new Firebase("https://winvin.firebaseio.com/news")
+    });
+  }
+});
+
+App.NewsItemRoute = Ember.Route.extend({
+  model: function(params) {
+    return EmberFire.Object.create({
+      ref: new Firebase("https://winvin.firebaseio.com/news/" + params.news_item_id)
+    });
+  }
 });
