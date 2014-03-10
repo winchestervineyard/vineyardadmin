571e022be1fb493af3886c0845d0749e2e886a20
Chris Parsons <chris@thinkcodelearn.com>
2014-03-10 11:19:05 +0000

    New Yeoman-based Angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('vineyardadminApp', [
+  'ngCookies',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
