052d202289488d80efdeee9c3df7c5bf422cdd4b
Chris Parsons <chris@thinkcodelearn.com>
2014-03-11 08:48:07 +0000

    Add angular fire

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,13 +3,22 @@
 angular.module('vineyardadminApp', [
   'ngCookies',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'firebase',
+  'angularfire.firebase',
+  'angularfire.login',
+  'simpleLoginTools'
 ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
+      })
+      .when('/login', {
+        authRequired: false, // if true, must log in before viewing this page
+        templateUrl: 'views/login.html',
+        controller: 'LoginController'
       })
       .otherwise({
         redirectTo: '/'
