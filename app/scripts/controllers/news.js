fb5d81d9aa63b0c8abf63bdd6fa207ba252a78c7
Chris Parsons <chris@thinkcodelearn.com>
2014-03-26 15:02:24 +0000

    Stop relying on object equality for editing

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -4,19 +4,25 @@ define(['controllers'], function(controllers) {
   controllers.controller('NewsController', function($scope, syncData) {
     syncData('news').$bind($scope, 'news');
 
-    $scope.edit = function(item) {
-      if (item.published) {
+    $scope.edit = function(id) {
+      $scope.id = id;
+      if ($scope.news[id].published) {
         $scope.errorMessage = 'Please unpublish this item before editing.';
+        $scope.editing = false;
         return;
       }
-      //$scope.errorMessage = '';
-      //item['edited_at'] = new Date();
-      //item['last_edited_by'] = $scope.auth.user.email;
-      $scope.subject = item;
+      $scope.editing = true;
+      $scope.errorMessage = '';
+      $scope.news[id]['edited_at'] = new Date();
+      $scope.news[id]['last_edited_by'] = $scope.auth.user.email;
     };
 
+    $scope.editingThis = function(item) {
+      return $scope.editing && $scope.news[$scope.id] == item;
+    }
+
     $scope.doneEditing = function() {
-      $scope.subject = null;
+      $scope.editing = false;
     };
   });
 });
